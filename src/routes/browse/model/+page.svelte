5463596ced90fa6fee75661d36d58a55642859cc
Antonio Cheong <acheong@student.dalat.org>
2022-11-24 17:53:17 +0800

    CSR and mobile optimization

--- src/routes/browse/model/+page.svelte
+++ src/routes/browse/model/+page.svelte
@@ -1,42 +1,44 @@
 <script lang="ts">
-	import SvelteMarkdown from 'svelte-markdown';
+  import SvelteMarkdown from "svelte-markdown";
 
-	// get model_readme from localStorage
-	async function getModel() {
-		const title = localStorage.getItem('model_title');
-		const readme = localStorage.getItem('model_readme');
-		const source = localStorage.getItem('model_source');
-		const download = localStorage.getItem('model_download');
-		const description = localStorage.getItem('model_description');
-		const author = localStorage.getItem('model_author');
-		return { title, readme, source, download, description, author };
-	}
+  // get model_readme from localStorage
+  async function getModel() {
+    const title = localStorage.getItem("model_title");
+    const readme = localStorage.getItem("model_readme");
+    const source = localStorage.getItem("model_source");
+    const download = localStorage.getItem("model_download");
+    const description = localStorage.getItem("model_description");
+    const author = localStorage.getItem("model_author");
+    return { title, readme, source, download, description, author };
+  }
 </script>
 
 <div class="container m-auto">
-	{#await getModel()}
-		<div>Loading...</div>
-	{:then model}
-		<div class="grid grid-cols-2 gap-4">
-			<div>
-				<article class="prose m-5">
-					<h1>{model.title}</h1>
-					<SvelteMarkdown source={model.readme} />
-				</article>
-			</div>
-			<div>
-				<div class="card w-96 bg-neutral text-neutral-content">
-					<div class="card-body">
-						<h2 class="card-title">{model.title}</h2>
-						<p>Author: {model.author}</p>
-						<p>{model.description}</p>
-						<div class="card-actions justify-end">
-							<a href={model.source} class="btn btn-primary">Source</a>
-							<a href={model.download}><button class="btn btn-secondary">Download</button></a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	{/await}
+  {#await getModel()}
+    <div>Loading...</div>
+  {:then model}
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-2">
+      <div class="md:col-span-1">
+        <div class="card bg-neutral text-neutral-content">
+          <div class="card-body">
+            <h2 class="card-title">{model.title}</h2>
+            <p>Author: {model.author}</p>
+            <p>{model.description}</p>
+            <div class="card-actions justify-end">
+              <a href={model.source} class="btn btn-primary">Source</a>
+              <a href={model.download}
+                ><button class="btn btn-secondary">Download</button></a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="md:col-span-2">
+        <article class="prose m-5">
+          <h1>{model.title}</h1>
+          <SvelteMarkdown source={model.readme} />
+        </article>
+      </div>
+    </div>
+  {/await}
 </div>
